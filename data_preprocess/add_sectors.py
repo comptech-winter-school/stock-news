8470a8c667eb390a54fa6438fd20e20aba539766
VinterMute <39826275+VinterMute@users.noreply.github.com>
2021-02-05 00:19:01 +0700

    Update add_sectors.py

--- data_preprocess/add_sectors.py
+++ data_preprocess/add_sectors.py
@@ -4,4 +4,4 @@ import pandas as pd
 def add_sectors(dataset,path_to_json):
     ticker_sector = pd.read_json(path_to_json).drop(['name', 'date'], axis=1)
 
-    return pd.merge(dataset,ticker_sector, left_on="Ticker",right_on="ticket")
+    return (pd.merge(dataset,ticker_sector, left_on="Ticker",right_on="ticket")).drop(["ticket"],axis=1)

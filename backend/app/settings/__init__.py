b0c0c5f38f97f6a36adbf1b738519a67ee6a060a
Valentin Mamedov <mamedov@promsoft.ru>
2021-02-06 13:39:43 +0800

    Fixed some staff

--- backend/app/settings/__init__.py
+++ backend/app/settings/__init__.py
@@ -1,14 +1,11 @@
-import yaml
-
-from app.settings.consts import SERVICE_NAME, CONFIG_DIR
-
 CONFIG = dict()
 
 
 def load_config():
-    with open(CONFIG_DIR / "app.yaml") as f:
-        CONFIG["app"] = yaml.safe_load(f)
+    pass
+# with open(CONFIG_DIR / "app.yaml") as f:
+#     CONFIG["app"] = yaml.safe_load(f)
 
-    # Delete what you don't need
-    # CONFIG["clickhouse"] = ClickHouse.read_settings_async()
-    # CONFIG["mysql"] = MySQL.read_settings_async()
+# Delete what you don't need
+# CONFIG["clickhouse"] = ClickHouse.read_settings_async()
+# CONFIG["mysql"] = MySQL.read_settings_async()

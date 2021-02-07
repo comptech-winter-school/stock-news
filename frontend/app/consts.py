2856b3cf2e0ed1d2a3dc115ef2c996d5fc68c931
Valentin Mamedov <mamedov@promsoft.ru>
2021-02-07 12:18:40 +0800

    Added wiki

--- frontend/app/consts.py
+++ frontend/app/consts.py
@@ -1,4 +1,4 @@
 
-# BACKEND_URL = "https://stock-news.site/backend"
-
-BACKEND_URL = "http://localhost:8080/backend"
+BACKEND_URL = "https://stock-news.site/backend"
+#
+# BACKEND_URL = "http://localhost:8080/backend"
